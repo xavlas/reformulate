c85d5fdbaeab82491ae46882da50474b8b0261a8
xavlas <lassus.xavier@gmail.com>
2025-07-28 15:57:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ auth_config = {
     'credentials': {
         'usernames': {
             'xavier': {
-                'name': st.secrets["users"]["xavier"]["name"],
-                'email': st.secrets["users"]["xavier"]["email"],
-                'password': st.secrets["users"]["xavier"]["password"],
+                'name': st.secrets["users.xavier"]["name"],
+                'email': st.secrets["users.xavier"]["email"],
+                'password': st.secrets["users.xavier"]["password"],
             }
         }
     },
