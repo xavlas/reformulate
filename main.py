95f15fb6557e86ee6f4920ac81e27ee76a3fc296
xavlas <lassus.xavier@gmail.com>
2025-07-29 09:53:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import streamlit as st
-if st.button("Log in"):
-    st.login("auth0")
-if st.user.is_logged_in:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.user.name}!)
+
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+st.title("Chat Assistant LogIn")
+
+if st.session_state["login"]:
+    st.success("LogIn Successfull")
+else:
+    password = st.text_input("Please enter the password", disabled=st.session_state.login)
+    if st.button("LogIn"):
+        if not password:
+            st.error("Please Type the Password", icon = "🚨")
+        else:
+            if password == st.secrets["PASSWORD"]:
+                st.session_state.login = True
+                st.toast("Login Successful 🤩")
+                st.success("LogIn Successfull")
+                st.subheader('Please visit the chatbot page!')
+            else:
+                st.toast("Incoreect Password 😶")
+                st.error('Incorrect Password', icon = "🚨")
